9c7f29dd5e493497a8f37e0325601e4e7ee4cbbd
Sergejs Visockis <sergei.visotsky@gmail.com>
2024-03-12 20:54:42 +0100

    provide an initial skeleton

---
+++ document-write-service/src/main/java/com/sergejs/documentservice/write/DocumentWriteApplication.java
@@ -0,0 +1,13 @@
+package com.sergejs.documentservice.write;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentWriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentWriteApplication.class, args);
+    }
+
+}
