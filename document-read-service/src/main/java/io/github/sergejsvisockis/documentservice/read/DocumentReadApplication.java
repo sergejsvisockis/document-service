637010eea0f105bfc60a9650db28a73be0b86f40
Sergejs Visockis <sergei.visotsky@gmail.com>
2024-03-14 09:37:16 +0100

    rename the package and dockerize

--- document-read-service/src/main/java/io/github/sergejsvisockis/documentservice/read/DocumentReadApplication.java
+++ document-read-service/src/main/java/io/github/sergejsvisockis/documentservice/read/DocumentReadApplication.java
@@ -1,4 +1,4 @@
-package com.sergejs.documentservice.read;
+package io.github.sergejsvisockis.documentservice.read;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- document-write-service/src/main/java/io/github/sergejsvisockis/documentservice/write/DocumentWriteApplication.java
+++ document-write-service/src/main/java/io/github/sergejsvisockis/documentservice/write/DocumentWriteApplication.java
@@ -1,4 +1,4 @@
-package com.sergejs.documentservice.write;
+package io.github.sergejsvisockis.documentservice.write;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
