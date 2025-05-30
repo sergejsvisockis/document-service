750150a4b02788050ce620a5c229c58cf00cc733
Sergejs Visockis <sergei.visotsky@gmail.com>
2025-05-30 15:16:54 +0200

    add DocGen mock stub app

---
+++ services/document-generation-mock/src/main/java/io/github/sergejsvisockis/docgen/mock/DocGenMockApplication.java
@@ -0,0 +1,12 @@
+package io.github.sergejsvisockis.docgen.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocGenMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocGenMockApplication.class, args);
+    }
+}
