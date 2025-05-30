50d2113f64513aaf0e71236bd6926ab38cad565c
Sergejs Visockis <sergei.visotsky@gmail.com>
2025-05-30 17:35:42 +0200

    DocGenMock -> DocGenService

--- services/document-generation-service/src/main/java/io/github/sergejsvisockis/docgen/mock/DocGenApplication.java
+++ services/document-generation-service/src/main/java/io/github/sergejsvisockis/docgen/mock/DocGenApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DocGenMockApplication {
+public class DocGenApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DocGenMockApplication.class, args);
+        SpringApplication.run(DocGenApplication.class, args);
     }
 }
